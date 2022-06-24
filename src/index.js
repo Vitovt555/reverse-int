0247f40d7682e4a3d1bf5e4d1f8777f7dc627150
Vitovt <zika.zika2013@mail.ru>
2022-06-24 19:42:49 +0200

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let res = []
+    const arr = n.toString().split('')
+    if(n<0) res = arr.splice(0,1).reverse().join('')
+    res = arr.reverse().join('')
+    return +res
 }
